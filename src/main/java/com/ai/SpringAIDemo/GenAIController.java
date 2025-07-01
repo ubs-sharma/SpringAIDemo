ab280403a52064b61a3578959f0981ec3b9f5780
banushasarma <sharmabanu03@gmail.com>
2025-07-01 20:02:46 +0530

    Recipe creator added

--- src/main/java/com/ai/SpringAIDemo/GenAIController.java
+++ src/main/java/com/ai/SpringAIDemo/GenAIController.java
@@ -15,10 +15,12 @@ import java.util.stream.Collectors;
 public class GenAIController {
     private final ChatService chatService;
     private final ImageService imageService;
+    private final RecipeService recipeService;
 
-    public GenAIController(ChatService chatService, ImageService imageService) {
+    public GenAIController(ChatService chatService, ImageService imageService, RecipeService recipeService) {
         this.chatService = chatService;
         this.imageService = imageService;
+        this.recipeService = recipeService;
     }
 
     @GetMapping("ask-ai")
@@ -52,5 +54,14 @@ public class GenAIController {
                 .map(result -> result.getOutput().getUrl())
                 .toList();
         return imageUrls;
+    }
+
+    @GetMapping("recipe-creator")
+    public String recipeCreator(
+            @RequestParam String ingredients,
+            @RequestParam(defaultValue = "any") String cuisine,
+            @RequestParam(defaultValue = "") String dietaryRestrictions
+    ) {
+        return recipeService.createReceipe(ingredients, cuisine, dietaryRestrictions);
     }
 }
