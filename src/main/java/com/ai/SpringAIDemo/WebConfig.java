bfcdd68212526fc5f7d89c6fe2d75908374d0072
banushasarma <sharmabanu03@gmail.com>
2025-07-03 20:17:00 +0530

    Cors added

---
+++ src/main/java/com/ai/SpringAIDemo/WebConfig.java
@@ -0,0 +1,21 @@
+package com.ai.SpringAIDemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:5173/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
